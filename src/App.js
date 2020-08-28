7c7a26653c89caeae8d76faf8c3b2455e5e4e4ac
Brian Kozub <ticet11@gmail.com>
2020-08-27 21:19:02 -0600

    added useEffect, so wordFetcher will run on render.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
     const [word, setWord] = useState('');
 
-    
+    useEffect(() => {
+        wordFetcher();
+    })
 
     const wordFetcher = () => {
         fetch(
